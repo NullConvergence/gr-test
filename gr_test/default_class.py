13990753f147fa11d3d15e33dba5ddd91db6f4d8
NullConvergence <a.serban@cs.ru.nl>
2020-05-15 01:22:53 +0200

    Extra line added in commit

--- gr_test/default_class.py
+++ gr_test/default_class.py
@@ -3,6 +3,10 @@ class Default(object):
         print("initialized default class")
         if name is not None:
             self.name = name
+        else:
+            self.name = "default_name"
+
+        print("Extra line added in commit.")
 
     def get_name(self):
         if self.name:
